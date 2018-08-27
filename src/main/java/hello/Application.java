8c81ce9b414e7ecb3731c1c5adf2b12cf4edd25a
Nicolas Brüggemann <nbrueggemann@itcoops.de>
2018-08-27 19:05:11 +0200

    hibernate test

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -19,6 +19,7 @@ public class Application {
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
 
+            /*
             System.out.println("Let's inspect the beans provided by Spring Boot:");
 
             String[] beanNames = ctx.getBeanDefinitionNames();
@@ -26,6 +27,7 @@ public class Application {
             for (String beanName : beanNames) {
                 System.out.println(beanName);
             }
+            */
 
         };
     }
